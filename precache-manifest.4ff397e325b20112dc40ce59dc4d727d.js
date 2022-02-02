be846ba4666acc4e5d20d3d33b18ce7b93868bff
Dayita_C <dayitachaudhuri.tech@gmail.com>
2022-02-02 14:41:24 +0530

    Updates

---
+++ precache-manifest.4ff397e325b20112dc40ce59dc4d727d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46a6b10968779aac3285",
+    "url": "/Portfolio/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "46a6b10968779aac3285",
+    "url": "/Portfolio/static/js/main.5230f361.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "7777a73f3a264166d890",
+    "url": "/Portfolio/static/js/2.25e1a7c1.chunk.js"
+  },
+  {
+    "revision": "cf26ced9db2897ea81f0eea94ebcf077",
+    "url": "/Portfolio/index.html"
+  }
+];
